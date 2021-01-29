0c3cd49ad3b58ff484f75ebab9008f95ff9a4329
Alex <a@b.com>
2021-01-29 23:04:22 +0000

    Main Menu and gloop

---
+++ Assets/Scripts/Gloop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gloop : MonoBehaviour
+{
+    public Transform respawnPosition;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<PlayerController>().movePoint.transform.position = respawnPosition.position;
+            collision.transform.position = collision.GetComponent<PlayerController>().movePoint.transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+
+    public AudioMixer musicMixer;
+
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    public void Back()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void Settings()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void SetMusicVolume(float vol)
+    {
+        musicMixer.SetFloat("MusicVol", Mathf.Log10(vol) * 20);
+    }
+
+    public void SetSFXVolume(float vol)
+    {
+        musicMixer.SetFloat("SFXVol", Mathf.Log10(vol) * 20);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Back();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
