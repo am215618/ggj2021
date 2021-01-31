e1468a8b31fe18ebb75784e97023c1689b2875c9
Daniel Tilbey <dt193807@falmouth.ac.uk>
2021-01-31 14:10:51 +0000

    added animation for magic power and implemented ability updates ui when you interact with pickups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public enum PlayerState { Normal, Dialogue }
 public class PlayerController : MonoBehaviour
 {
+    float spawnDistance = 0.32f;
+    public GameObject magicPowerPrefab;
+    bool isAttacking = false;
+    public bool hasAbility;
+    public bool hasKey;
     public PlayerState state;
 
     public Animator animator;
@@ -31,106 +36,127 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         if (state == PlayerState.Normal)
-        {
-            //movePoint.transform.position = new Vector3(Input.GetAxis("Horizontal") * gridCellSize, Input.GetAxis("Vertical") * gridCellSize, 0f));
-
-            //if (Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, collisionLayer))
-            transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed * Time.deltaTime);
-
-            if (transform.position == movePoint.position)
             {
-                float hMove = Input.GetAxisRaw("Horizontal");
-                float vMove = Input.GetAxisRaw("Vertical");
+                //movePoint.transform.position = new Vector3(Input.GetAxis("Horizontal") * gridCellSize, Input.GetAxis("Vertical") * gridCellSize, 0f));
 
-                animator.SetFloat("HorizontalDirection", hMove);
-                animator.SetFloat("VerticalDirection", vMove);
+                //if (Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, collisionLayer))
+                transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed * Time.deltaTime);
 
-                if (hMove == 0 && vMove == 0)
+            if (!isAttacking) {
+                if (transform.position == movePoint.position)
                 {
-                    animator.SetBool("InputPressed", false);
-                }
-                if (hMove == 0)
-                {
-                    hInput = false;
-                }
-                if (vMove == 0)
-                {
-                    vInput = false;
-                }
+                    float hMove = Input.GetAxisRaw("Horizontal");
+                    float vMove = Input.GetAxisRaw("Vertical");
 
-                if (hMove == 0 && vMove == 0)
-                {
-                    animator.SetBool("InputPressed", false);
-                    vInput = false;
-                    hInput = false;
-                }
+                    animator.SetFloat("HorizontalDirection", hMove);
+                    animator.SetFloat("VerticalDirection", vMove);
 
-                if (Vector3.Distance(transform.position, movePoint.position) <= 0.16f)
-                {
-                    if (!vInput)
+                    if (hMove == 0 && vMove == 0)
                     {
-                        if (Mathf.Abs(hMove) == 1f)
-                        {
-                            hInput = true;
-                            if (hMove == 1)
-                            {
-                                animator.SetBool("LastPressedRight", true);
-                                animator.SetBool("LastPressedLeft", false);
-                                animator.SetBool("LastPressedUp", false);
-                                animator.SetBool("LastPressedDown", false);
-                            }
-                            else
-                            {
-                                animator.SetBool("LastPressedRight", false);
-                                animator.SetBool("LastPressedLeft", true);
-                                animator.SetBool("LastPressedUp", false);
-                                animator.SetBool("LastPressedDown", false);
-                            }
+                        animator.SetBool("InputPressed", false);
+                    }
+                    if (hMove == 0)
+                    {
+                        hInput = false;
+                    }
+                    if (vMove == 0)
+                    {
+                        vInput = false;
+                    }
 
-                            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal") * gridCellSize, 0f, 0f), .12f, collisionLayer))
-                            {
-                                animator.SetBool("InputPressed", true);
-                                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal") * gridCellSize, 0f, 0f);
+                    if (hMove == 0 && vMove == 0)
+                    {
+                        animator.SetBool("InputPressed", false);
+                        vInput = false;
+                        hInput = false;
+
+                        if (Input.GetKeyUp(KeyCode.A) && hasAbility)
+                        {
+                            if (animator.GetBool("LastPressedRight")) { 
+                                Instantiate(magicPowerPrefab, new Vector3(gameObject.transform.position.x + spawnDistance, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+                            }
+                            if (animator.GetBool("LastPressedLeft")) { 
+                                Instantiate(magicPowerPrefab, new Vector3(gameObject.transform.position.x - spawnDistance, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+                            }
+                            if (animator.GetBool("LastPressedUp")) { 
+                                Instantiate(magicPowerPrefab, new Vector3(gameObject.transform.position.y + spawnDistance, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+                            }
+                            if (animator.GetBool("LastPressedDown")) { 
+                                Instantiate(magicPowerPrefab, new Vector3(gameObject.transform.position.y - spawnDistance, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
                             }
                         }
                     }
 
-                    if (!hInput)
+                    if (Vector3.Distance(transform.position, movePoint.position) <= 0.16f)
                     {
-                        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+                        if (!vInput)
                         {
-                            vInput = true;
-                            if (vMove == 1)
+                            if (Mathf.Abs(hMove) == 1f)
                             {
-                                animator.SetBool("LastPressedRight", false);
-                                animator.SetBool("LastPressedLeft", false);
-                                animator.SetBool("LastPressedUp", true);
-                                animator.SetBool("LastPressedDown", false);
-                            }
-                            else
-                            {
-                                animator.SetBool("LastPressedRight", false);
-                                animator.SetBool("LastPressedLeft", false);
-                                animator.SetBool("LastPressedUp", false);
-                                animator.SetBool("LastPressedDown", true);
-                            }
-                            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical") * gridCellSize, 0f), .12f, collisionLayer))
-                            {
-                                animator.SetBool("InputPressed", true);
-                                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical") * gridCellSize, 0f);
+                                if (hMove == 1)
+                                {
+                                    animator.SetBool("LastPressedRight", true);
+                                    animator.SetBool("LastPressedLeft", false);
+                                    animator.SetBool("LastPressedUp", false);
+                                    animator.SetBool("LastPressedDown", false);
+                                }
+                                else
+                                {
+                                    animator.SetBool("LastPressedRight", false);
+                                    animator.SetBool("LastPressedLeft", true);
+                                    animator.SetBool("LastPressedUp", false);
+                                    animator.SetBool("LastPressedDown", false);
+                                }
+
+                                hInput = true;
+
+
+                                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal") * gridCellSize, 0f, 0f), .12f, collisionLayer))
+                                {
+                                    animator.SetBool("InputPressed", true);
+                                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal") * gridCellSize, 0f, 0f);
+                                }
                             }
                         }
-                    }
 
-                    if (Physics2D.OverlapCircle(movePoint.position, .32f, doorLayer))
-                    {
-                        foreach (Door door in doors)
+                        if (!hInput)
                         {
-                            float distance = Vector2.Distance(door.transform.position, transform.position);
-
-                            if (distance <= .32f)
+                            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
                             {
-                                door.OpenDoor();
+                                if (vMove == 1)
+                                {
+                                    animator.SetBool("LastPressedRight", false);
+                                    animator.SetBool("LastPressedLeft", false);
+                                    animator.SetBool("LastPressedUp", true);
+                                    animator.SetBool("LastPressedDown", false);
+                                }
+                                else
+                                {
+                                    animator.SetBool("LastPressedRight", false);
+                                    animator.SetBool("LastPressedLeft", false);
+                                    animator.SetBool("LastPressedUp", false);
+                                    animator.SetBool("LastPressedDown", true);
+                                }
+                                
+                                vInput = true;
+                                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical") * gridCellSize, 0f), .12f, collisionLayer))
+                                {
+                                    animator.SetBool("InputPressed", true);
+                                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical") * gridCellSize, 0f);
+                                }
+                            }
+                        }
+
+                        if (Physics2D.OverlapCircle(movePoint.position, .32f, doorLayer))
+                        {
+                            foreach (Door door in doors)
+                            {
+                                float distance = Vector2.Distance(door.transform.position, transform.position);
+
+                                if (distance <= .32f)
+                                {
+                                    door.OpenDoor();
+                                }
                             }
                         }
                     }
