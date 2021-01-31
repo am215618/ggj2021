02867d9d63cd74b40bf76c1539206131af5f27ab
Alex <a@b.com>
2021-01-31 15:48:24 +0000

    sound transition

---
+++ Assets/Scripts/SoundTransition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTransition : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip audioClip;
+    public AudioClip ogaudioClip;
+
+    public void ChangeSong()
+    {
+        source.clip = audioClip;
+        source.Play();
+    }
+}
